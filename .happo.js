cd3776b67640bad86cd73d7c00f7a1b1dd92f342
Mateusz Bloch <mateusz.bloch@cognifide.com>
2021-01-27 13:24:04 +0100

    Some changes in browsersTarget

--- .happo.js
+++ .happo.js
@@ -3,9 +3,24 @@ const { RemoteBrowserTarget } = require('happo.io');
 module.exports = {
     apiKey: '3f753656fa',
     apiSecret: '4ca6911d669f2b0418334a628',
-  targets: {
-    chrome: new RemoteBrowserTarget('chrome', {
-      viewport: '1024x768',
-    }),
-  },
-};
+    targets: {
+      chrome: new RemoteBrowserTarget('chrome', {
+        viewport: '1920x1200',
+      }),
+      'chrome-mobile': new RemoteBrowserTarget('chrome', {
+        viewport: '375x768',
+      }),
+      safari: new RemoteBrowserTarget('safari', {
+        viewport: '1920x1200',
+      }),
+      firefox: new RemoteBrowserTarget('firefox', {
+        viewport: '1920x1200',
+      }),
+      iossafari: new RemoteBrowserTarget('ios-safari', {
+        viewport: '1334x750',
+      }),
+      'internet explorer': new RemoteBrowserTarget('internet explorer', {
+        viewport: '1920x1200',
+      }),
+    },
+  };
