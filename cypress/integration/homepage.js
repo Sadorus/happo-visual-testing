1ab787d6a9fda92a75741774ef0e6ba9ff48724c
Mateusz Bloch <mateusz.bloch@cognifide.com>
2021-02-08 15:29:23 +0100

    h

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,10 +1,28 @@
 describe('Home page', function () {
     it('loads properly', function () {
-      cy.visit('https://www.astonmartinartofliving.com/en');
+      cy.visit('https://wttech.blog/');
 
 
       cy.wait(3500)
       cy.get('body')
       .happoScreenshot()
+      
+
+      cy.wait(1500)
+      cy.happoHideDynamicElements({
+        selectors: ['.page-title'],
+      });
+
+      cy.get('body')
+      .happoScreenshot()
+
+      cy.get('header.header')
+      .happoScreenshot()
+
+      cy.get('a.navbar-item.relative.hover-border-bottom-swipe-in.pseudo-bg-color-open-source')
+      .click()
+      .get('body')
+      .happoScreenshot()
+
     });
   });
