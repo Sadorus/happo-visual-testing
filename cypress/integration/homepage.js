8df6b84a5cefa4d4090056212261204f7ab1452a
Mateusz Bloch <mateusz.bloch@cognifide.com>
2021-02-08 15:05:30 +0100

    Small changes

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -6,23 +6,5 @@ describe('Home page', function () {
       cy.wait(3500)
       cy.get('body')
       .happoScreenshot()
-      
-
-//      cy.wait(2500)
-//      cy.happoHideDynamicElements({
-//        selectors: ['.page-subtitle'],
-//      });
-
-      cy.get('body')
-      .happoScreenshot()
-
-//      cy.get('header.header')
-//      .happoScreenshot()
-
-//      cy.get('a.navbar-item.relative.hover-border-bottom-swipe-in.pseudo-bg-color-open-source')
-//      .click()
-//      .get('body')
-//      .happoScreenshot()
-
     });
   });
