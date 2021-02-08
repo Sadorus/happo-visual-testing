8660f761fd7c8b7deb36a377320c659a315c6c67
Mateusz Bloch <mateusz.bloch@cognifide.com>
2021-02-08 14:57:45 +0100

    asto test

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,6 +1,6 @@
 describe('Home page', function () {
     it('loads properly', function () {
-      cy.visit('https://wttech.blog/');
+      cy.visit('https://www.astonmartinartofliving.com/en/');
 
 
       cy.wait(3500)
@@ -8,21 +8,21 @@ describe('Home page', function () {
       .happoScreenshot()
       
 
-      cy.wait(2500)
-      cy.happoHideDynamicElements({
-        selectors: ['.page-subtitle'],
-      });
+//      cy.wait(2500)
+//      cy.happoHideDynamicElements({
+//        selectors: ['.page-subtitle'],
+//      });
 
       cy.get('body')
       .happoScreenshot()
 
-      cy.get('header.header')
-      .happoScreenshot()
+//      cy.get('header.header')
+//      .happoScreenshot()
 
-      cy.get('a.navbar-item.relative.hover-border-bottom-swipe-in.pseudo-bg-color-open-source')
-      .click()
-      .get('body')
-      .happoScreenshot()
+//      cy.get('a.navbar-item.relative.hover-border-bottom-swipe-in.pseudo-bg-color-open-source')
+//      .click()
+//      .get('body')
+//      .happoScreenshot()
 
     });
   });
